f6fe4ec742976b1b04fe0a04a2acb009b4491bcd
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-12-06 18:32:57 -0300

    adding mask for security-code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import "./css/index.css"
+import "./css/index.css";
+import IMask from 'imask';
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
@@ -18,3 +19,11 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector("#security-code");
+const securityCodePattern = {
+    mask: "0000"
+};
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
